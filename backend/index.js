51ca0e550fc84ce7976df7cb33fa906be425e8c1
vedantpawar18 <vedantpawar18@gmail.com>
2022-12-03 21:58:41 +0530

    student model authentication and route created

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ var cors = require('cors')
 app.use(express.json());
 app.use(cors());
 
-var logRouter=require("./Routes/Login.route")
+var logRouter=require("./Routes/Login.route");
+var studentRouter=require("./Routes/Student.route")
 
 const Connection=require("./Config/db")
 
-app.use("/auth",logRouter)
+app.use("/auth",logRouter);
+app.use("/student",studentRouter);
 
 app.get("/", (req,res)=>{
     res.send("Welcome to homepage");
