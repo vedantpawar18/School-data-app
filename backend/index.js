e19940319aa6ded7b2b588cf22109b602f9a8c7e
vedantpawar18 <vedantpawar18@gmail.com>
2022-12-03 18:29:29 +0530

    connected to mongoDb

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,19 @@ const express=require("express");
 const app=express();
 app.use(express.json());
 
+const Connection=require("./Config/db")
+
 
 app.get("/", (req,res)=>{
     res.send("Welcome to homepage");
 });
 
-app.listen(8080,()=>{
-    console.log("port started at 8080")
+app.listen(8080,async()=>{
+    try{
+        await Connection;
+        console.log("port started at 8080");
+    }
+    catch(err){
+        console.log(err)
+    }
 })
